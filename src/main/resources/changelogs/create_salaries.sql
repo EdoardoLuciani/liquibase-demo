bab46d8f0dd1bc2ead384f4a02e578a5c6978cb8
EdoardoLuciani <edoardo.luciani@gmail.com>
2023-11-12 23:34:53 +0000

    Add salary_id column to employee table

--- src/main/resources/changelogs/create_salaries.sql
+++ src/main/resources/changelogs/create_salaries.sql
@@ -5,5 +5,8 @@ CREATE TABLE salary (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `amount` INTEGER NOT NULL
 );
-
 --rollback DROP TABLE IF EXISTS salary;
+
+--changeset edoardo:5
+ALTER TABLE employee ADD `salary_id` INTEGER;
+--rollback ALTER TABLE employee DROP COLUMN salary_id;
