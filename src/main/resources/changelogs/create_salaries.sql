3278e7e5772b88fb5b3c691944d55b6e61d25f74
EdoardoLuciani <edoardo.luciani@gmail.com>
2023-11-13 00:11:31 +0000

    Add salary migration to another table

--- src/main/resources/changelogs/create_salaries.sql
+++ src/main/resources/changelogs/create_salaries.sql
@@ -10,3 +10,16 @@ CREATE TABLE salary (
 --changeset edoardo:5
 ALTER TABLE employee ADD `salary_id` INTEGER;
 --rollback ALTER TABLE employee DROP COLUMN salary_id;
+
+--changeset edoardo:6
+INSERT INTO salary (amount) SELECT distinct salary FROM employee;
+--rollback DELETE FROM salary;
+
+--changeset edoardo:7
+update employee set salary_id = s.id from employee e1 left join salary s on e1.salary = s.amount where employee.id = e1.id
+--rollback update employee set salary_id = null;
+
+--changeset edoardo:8
+ALTER TABLE employee DROP COLUMN salary;
+--rollback ALTER TABLE employee ADD salary INTEGER;
+--rollback update employee set salary = s.amount from employee e1 left join salary s on e1.salary_id  = s.id  where employee.id = e1.id;
